e3c5f88294b19b055e576b77bd8de349bcec4202
dalston <xxdalston@gmail.com>
2021-01-24 23:50:52 +0800

    feat: update week16

---
+++ Week 16/gesture/webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  entry: "./gesture.js",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  mode: "development"
+}
