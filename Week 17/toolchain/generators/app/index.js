ffde1633e941de4a02b1fc292365475015ce2a2b
dalston <xxdalston@gmail.com>
2021-02-01 01:22:18 +0800

    feat: babel, toolchain

--- Week 17/toolchain/generators/app/index.js
+++ Week 17/toolchain/generators/app/index.js
@@ -7,11 +7,26 @@ module.exports = class extends Generator {
     super(args, opts);
 
   } 
-  method1() {
+  async method1() {
     this.log('method 1 just ran');
-  }
 
-  method2() {
-    this.log('method 2 just ran');
+
+     const answers = await this.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Your project name",
+        default: this.appname // Default to current folder name
+      },
+      {
+        type: "confirm",
+        name: "cool",
+        message: "Would you like to enable the Cool feature?"
+      }
+    ]);
+
+    this.log("app name", answers.name);
+    this.log("cool feature", answers.cool);
   }
+ 
 };
